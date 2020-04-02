d30c535ed1620c34b9c4e9d7d25cf51fedf6f5bf
Japhet Teklu <yaphetmesfin@yahoo.com>
2020-04-02 20:16:44 +0200

    round 3

--- public/pizza.js
+++ public/pizza.js
@@ -1,24 +1,44 @@
 function eventList() {
-  const butts = document.querySelectorAll('button');
-  
-  butts.forEach(item => {
+  window.onunload = function() {
+    const myInit =  {
+      method: 'PUT',
+    }
+    fetch('/reset', myInit);
+  }
+  const buttons = document.querySelectorAll('button');
+  let pizzaCounter = 0;
+  buttons.forEach(item => {
     item.addEventListener('click', () => {
-      const myInit = {
+      pizzaCounter += 1;
+      const myInit1 = {
         method: 'POST',
         headers: {
           'Accept': 'application/json',
           'Content-Type': 'application/json'
         },
-        body: JSON.stringify({id: item.id})
+        body: JSON.stringify({value: item.value})
       }
-      fetch('/append', myInit).then(response => {
+      fetch('/append', myInit1).then(response => {
         return response.json();
       }).then(data => {
           document.querySelector(`p[id="${data.id}"]`).textContent = data.amount;
-        ;
-      })
-    })
-  })
+          document.querySelector('.final-number').textContent = " " + pizzaCounter;
+          if (data.amount === 1) {
+            document.querySelector('.inner-cart').innerHTML += `<p><span id=${data.name}>${data.amount}</span> ${data.name}</p>`;
+            document.querySelector('.final-pizzas').style.display = 'inline';
+            document.querySelector('.final-number').style.display = 'inline';
+            document.querySelector('.make-order').style.display = 'block';
+          }
+          else {
+            document.querySelector(`#${data.name}`).textContent = data.amount;
+          }
+          document.querySelector('.make-order').addEventListener('click', () => {
+            document.querySelector('.make-order').style = 'disabled';
+            document.querySelector('.order-message').style.display = 'block';
+          });
+      });
+    });
+  });
 }
 
 eventList();
--- server.js
+++ server.js
@@ -16,25 +16,31 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
+  console.log("second load");
   res.sendFile(path.join(__dirname+'/public/index.html'));
 });
 
-app.post('/pizza', (req, res) => {
-  fs.appendFile('mypizzafile.txt', 'Hii there', function (err) {
-    if (err) throw err;
+// app.post('/pizza', (req, res) => {
+//   fs.appendFile('mypizzafile.txt', 'Hii there', function (err) {
+//     if (err) throw err;
+//   });
+//   res.status(200).end();
+// });
+
+app.put('/reset', (req, res) => {
+  console.log('it logs')
+  pizzas.forEach(x => {
+    return x.amount = 0;
   });
   res.status(200).end();
-});
+})
 
 app.post('/append', (req, res) => {
-  pizzas[req.body.id].amount += 1; 
-  console.log(pizzas);
-  // pizzas[0].amount += 1;
-  // res.send(pizzas);
+  pizzas[req.body.value].amount += 1; 
   res.status(200)
-  res.send(pizzas[req.body.id]);
+  res.send(pizzas[req.body.value]);
 })
 
 app.listen(8000, () => {
-  console.log('Example app listening on port 8000!')
+  console.log('App listening on port 8000!')
 });
