5872c56abedf00673fcdba6d1119a451bcdf9d57
Japhet <yaphetmesfin@yahoo.com>
2020-05-30 22:04:04 +0200

    work in progress

--- public/pizza.js
+++ public/pizza.js
@@ -1,67 +1,74 @@
 function eventList() {
-  window.onunload = function () {
-    const myInit = {
-      method: 'PUT',
-    };
-    fetch('/reset', myInit);
-  };
-  const buttons = document.querySelectorAll('.butt-order');
-  let pizzaCounter = 0;
-  buttons.forEach((item) => {
-    item.addEventListener('click', () => {
-      pizzaCounter += 1;
-      const myInit1 = {
-        method: 'PUT',
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ value: item.value }),
-      };
-      fetch('/append', myInit1).then((response) => response.json())
-        .then((data) => {
-          document.querySelector(`p[id="${data.id}"]`).textContent = data.amount;
-          document.querySelector('.final-number').textContent = ` ${pizzaCounter}`;
-          if (data.amount === 1) {
-            document.querySelector('.inner-cart').innerHTML += `<p><span id=${data.name}>${data.amount}</span> ${data.name}</p>`;
-            document.querySelector('.final-pizzas').style.display = 'inline';
-            document.querySelector('.final-number').style.display = 'inline';
-            document.querySelector('.make-order').style.display = 'block';
-            document.querySelectorAll('.remove')[data.id].style.display = 'inline';
-          } else {
-            document.querySelector(`#${data.name}`).textContent = data.amount;
-          }
-          document.querySelector('.make-order').addEventListener('click', () => {
-            document.querySelector('.make-order').style = 'disabled';
-            document.querySelector('.order-message').style.display = 'block';
-          });
+  window.onunload = loadSummary();
+  
+  function loadSummary() { 
+    const summaryPizza = document.querySelector('.summary-pizza');
+    const localStoragePizza = JSON.parse(localStorage.getItem('pizzas'));
+    for (const pizzaType in localStoragePizza) {
+      const counterElement = document.createElement('p');
+      const trashButton = document.createElement('i');
+      trashButton.classList.add('fas');
+      trashButton.classList.add('fa-trash-alt');
+      trashButton.classList.add(pizzaType);
+      
+      counterElement.classList.add(`${pizzaType}-count`);
+      if (document.querySelector(`.${pizzaType}-count`)) {
+        document.querySelector(`.${pizzaType}-count`).textContent = `${pizzaType[0].toUpperCase()}${pizzaType.slice(1)} x ${localStoragePizza[pizzaType]}`;
+        document.querySelector(`.${pizzaType}-count`).appendChild(trashButton);
+        if (localStoragePizza[pizzaType] === 0) {
+          document.querySelector(`.${pizzaType}`).style.display = 'none';
+        }
+        trashButton.addEventListener('click', (e) => {
+          const deleteOnePizza = e.target.parentElement.className.split('-')[0];
+          const pizza = JSON.parse(localStorage.getItem('pizzas'));
+          pizza[deleteOnePizza] -= 1;
+          localStorage.setItem('pizzas', JSON.stringify(pizza));
+          loadSummary();
+          if (pizza[deleteOnePizza] === 0) {
+            document.querySelector(`.${deleteOnePizza}`).style.display = 'none';
+          } 
         });
+      } else {
+        counterElement.textContent = `${pizzaType[0].toUpperCase()}${pizzaType.slice(1)} x ${localStoragePizza[pizzaType]}`;
+        counterElement.appendChild(trashButton);
+        summaryPizza.appendChild(counterElement);
+        // loadSummary();
+        document.querySelector('.make-order').style.display = 'block';
+        document.querySelector('.summary-amount').style.display = 'block';
+        let totalOrder = 0;
+        for (const item in localStoragePizza) {
+          totalOrder += localStoragePizza[item];
+        }
+        // document.querySelector('.summary-amount').textContent += ' ' totalOrder;
+      }
+    }
+  }
+
+  const pizzaButtons = document.querySelectorAll('.button');
+  pizzaButtons.forEach(element => {
+    element.addEventListener('click', (event) => {
+      const localStoragekey = event.target.className.split('-')[0];
+      if (!(JSON.parse(localStorage.getItem('pizzas')))) {
+        localStorage.setItem('pizzas', JSON.stringify({}));
+      }
+      if (!((JSON.parse(localStorage.getItem('pizzas')))[localStoragekey])) {
+        const pizzas = JSON.parse(localStorage.getItem('pizzas'));
+        pizzas[localStoragekey] = 1;
+        localStorage.setItem('pizzas', JSON.stringify(pizzas));
+      } else {
+        const pizzas = JSON.parse(localStorage.getItem('pizzas'));
+        pizzas[localStoragekey] += 1;
+        localStorage.setItem('pizzas', JSON.stringify(pizzas));
+      }
+      loadSummary();
     });
   });
-  const removeButton = document.querySelectorAll('.remove');
-  removeButton.forEach(button => {
-    button.addEventListener('click', () => {
-      pizzaCounter -= 1;
-      fetch('/remove', {
-        method: 'PUT',
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ id: button.value }),
-      })
-        .then((response) => response.json())
-          .then(data => {
-            document.querySelector(`p[id="${data.id}"]`).textContent = data.amount;
-            document.querySelector(`span[id="${data.name}"]`).textContent = data.amount;
-            document.querySelector('.final-number').textContent = pizzaCounter;
-            if (data.amount === 0) {
-              document.querySelector(`.${data.name}`).style.display = 'none';
-              
-            }
-          })
-    })
-   })
+  
+  const makeOrder = document.querySelector('.make-order');
+  makeOrder.addEventListener('click', () => {
+
+  })
+
 }
 
 eventList();
--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const app = express();
+const Pool = require('pg').Pool;
 const path = require('path');
 const fs = require('fs');
 const bodyParser = require('body-parser');
-
-let pizzas = [
-  { id: 0, name: 'pepperoni', amount: 0 },
-  { id: 1, name: 'margherita', amount: 0 },
-  { id: 2, name: 'bbq_chicken', amount: 0 }
-]
-
+const pool = new Pool({
+  user: 'japhet',
+  host: 'localhost',
+  database: 'little_amsterdam',
+  password: 'password',
+  port: 5432,
+})
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -19,24 +20,14 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname+'/public/index.html'));
 });
 
-app.put('/remove', (req, res) => {
-  pizzas[req.body.id].amount -= 1;
-  res.status(200);
-  res.send(pizzas[req.body.id]);
-})
-
-app.put('/reset', (req, res) => {
-  pizzas.forEach(x => {
-    return x.amount = 0;
-  });
-  res.status(200).end();
-})
-
-app.put('/append', (req, res) => {
-  pizzas[req.body.value].amount += 1; 
-  res.status(200)
-  res.send(pizzas[req.body.value]);
-})
+const addOrder = (req, res) => {
+  pool.query('INSERT INTO orders (date, pizzatype) VALUES ($1, $2)', ['2020-05-29', 'chicken'],(error, result) => {
+    if (error) {
+      throw error
+    }
+  })
+}
+addOrder();
 
 app.listen(8000, () => {
   console.log('App listening on port 8000!')
