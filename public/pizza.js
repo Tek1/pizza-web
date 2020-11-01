af97b4f3e1d72f7afeae29abc1e87ded9c152a9a
Japhet <yaphetmesfin@yahoo.com>
2020-11-01 09:56:36 +0100

    code modified to MVC architecture

--- public/pizza.js
+++ public/pizza.js
@@ -1,79 +1,122 @@
-function eventList() {
-  window.onunload = loadSummary();
-  function loadSummary() { 
-    const summaryPizza = document.querySelector('.summary-pizza');
-    const localStoragePizza = JSON.parse(localStorage.getItem('pizzas'));
-    for (const pizzaType in localStoragePizza) {
-      if (localStoragePizza[pizzaType] === 0) {
-        return;
-      }
-    }
-    for (const pizzaType in localStoragePizza) {
-      if(!document.querySelector(`.${pizzaType}-summary`)) {
-        const summaryElement = document.createElement('p');
-        const counterElement = document.createElement('span');
-        const trashButton = document.createElement('i'); 
-        summaryElement.classList.add(`${pizzaType}-summary`);
-        counterElement.classList.add(`${pizzaType}-counter`);
-        trashButton.classList.add('fas');
-        trashButton.classList.add('fa-trash-alt');
-        trashButton.classList.add(pizzaType);
-        trashButton.addEventListener('click', (e) => {
-          const deleteOnePizza = e.target.parentElement.className.split('-')[0];
-          const pizza = JSON.parse(localStorage.getItem('pizzas'));
-          pizza[deleteOnePizza] -= 1;
-          localStorage.setItem('pizzas', JSON.stringify(pizza));
-          document.querySelector(`.${pizzaType}-counter`).textContent = pizza[pizzaType];
-          if (pizza[pizzaType] === 0) {
-            document.querySelector(`.${pizzaType}-summary`).remove();
-          }
-        });
-        summaryElement.classList.add(`${pizzaType}-summary`);
-        counterElement.classList.add(`${pizzaType}-counter`);
-        counterElement.textContent = localStoragePizza[pizzaType]; 
-        summaryElement.textContent = `${pizzaType[0].toUpperCase()}${pizzaType.slice(1)} x`;
-        summaryElement.appendChild(counterElement);
-        summaryElement.appendChild(trashButton);
-        summaryPizza.appendChild(summaryElement);
-        
-      } else {
-        document.querySelector(`.${pizzaType}-counter`).textContent = localStoragePizza[pizzaType]; 
-      }
-      
-
-        // let totalOrder = 0;
-        // for (const item in localStoragePizza) {
-        //   totalOrder += localStoragePizza[item];
-        // }
-      
-    }
+class Model {
+  constructor() {
+    this.cart = JSON.parse(localStorage.getItem('cart')) || {}
   }
 
-  const pizzaButtons = document.querySelectorAll('.button');
-  pizzaButtons.forEach(element => {
-    element.addEventListener('click', (event) => {
-      const localStoragekey = event.target.className.split('-')[0];
-      if (!(JSON.parse(localStorage.getItem('pizzas')))) {
-        localStorage.setItem('pizzas', JSON.stringify({}));
-      }
-      if (!((JSON.parse(localStorage.getItem('pizzas')))[localStoragekey])) {
-        const pizzas = JSON.parse(localStorage.getItem('pizzas'));
-        pizzas[localStoragekey] = 1;
-        localStorage.setItem('pizzas', JSON.stringify(pizzas));
-      } else {
-        const pizzas = JSON.parse(localStorage.getItem('pizzas'));
-        pizzas[localStoragekey] += 1;
-        localStorage.setItem('pizzas', JSON.stringify(pizzas));
-      }
-      loadSummary();
-    });
-  });
-  
-  const makeOrder = document.querySelector('.make-order');
-  makeOrder.addEventListener('click', () => {
+  bindHandleOnCartChange(handler) {
+    this.onCartChange = handler;
+  }
 
-  });
+  saveOrderType(orderType) {
+    this.orderType = orderType;
+  }
+
+  addToCart(item, size) {
+    if (this.cart[item]) {
+      if (this.cart[item][size]) {
+        this.cart[item][size] += 1;
+      } else {
+        this.cart[item][size] = 1;
+      }
+    } else {
+      this.cart[item] = {};
+      this.cart[item][size] = 1;
+    }
+    this.onCartChange(this.cart);
+  }
 
 }
 
-eventList();
+ class View {
+  constructor() {
+    this.summaryOrder = document.querySelector('.summary-order')
+    this.summaryAmount = document.querySelector('.summary-amount')
+    this.initListeners()
+  }
+
+  bindOrderOptions(handler) {
+    this.handleOrderType = handler
+  }
+
+  bindHandleAddToCart(handler) {
+    this.handleAddToCart = handler
+  }
+
+  displayCartItems(cart) {
+    while(this.summaryOrder.firstChild) {
+      this.summaryOrder.removeChild(this.summaryOrder.firstChild);
+    }
+
+    for (const item of Object.keys(cart)) {
+      for (const size of Object.keys(cart[item])) {
+        const itemLine = document.createElement('p');
+        const itemCount = document.createElement('span');
+        const itemNameAndSize = document.createElement('span');
+        const addItem = document.createElement('i');
+        const subtractItem = document.createElement('i');
+        addItem.classList.add('fas');
+        addItem.classList.add('fa-plus-circle');
+        subtractItem.classList.add('fas');
+        subtractItem.classList.add('fa-minus-circle') 
+        itemCount.textContent = ` ${cart[item][size]}`;
+        itemNameAndSize.textContent = ` ${size} ${item}`;
+        itemLine.append(addItem, itemCount, subtractItem, itemNameAndSize);
+        this.summaryOrder.append(itemLine);
+      }
+    }
+  }
+
+  initListeners() {
+    document.querySelectorAll('.order-option').forEach(button => {
+      button.addEventListener('click', event => {
+        const id = event.target.id;
+          this.handleOrderType(id);
+          this.hideOrderOptions();
+          document.querySelector('.left-half').style.margin = '0px';
+          document.querySelector('.cart').style.display = 'block';
+      });
+    });
+
+    document.querySelectorAll('.item-button').forEach(button => {
+      button.addEventListener('click', event => {
+        const id = event.target.id;
+        const size = document.getElementById(`${id}-size`).value;
+        this.handleAddToCart(id, size);
+      });
+    });
+  }
+
+  hideOrderOptions() {
+    document.querySelector('.order-options').style.display = 'none';
+  }
+}
+
+class Controller {
+  constructor(model, view) {
+    this.model = model
+    this.view = view
+
+    //  explicit this binding.
+    this.view.bindOrderOptions(this.handleOrderType)
+    this.view.bindHandleAddToCart(this.handleAddToCart)
+    this.model.bindHandleOnCartChange(this.handleOnCartChange)
+    
+    // display initial cart items
+    this.view.displayCartItems(this.model.cart)
+
+  }
+
+  handleOnCartChange = cart => {
+    this.view.displayCartItems(cart);
+  } 
+
+  handleOrderType = orderType => {
+    this.model.saveOrderType(orderType)
+  }
+
+  handleAddToCart = (item, size) => {
+    this.model.addToCart(item, size);
+  }
+}
+
+app = new Controller(new Model(), new View());
